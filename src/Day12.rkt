bea8d6989f7a33895f6ef0244440c17d174641ce
Igor Marta <mr.igor.marta@gmail.com>
2021-01-06 21:59:18 +0000

    [AoC-2020] - Code polish.

--- src/Day12.rkt
+++ src/Day12.rkt
@@ -1,10 +1,17 @@
 #lang racket
-(require (only-in threading λ~>> ~>>) "util/ParseUtils.rkt")
+(require (only-in racket/generic define-generics)
+         (only-in threading λ~>> ~>>)
+         "util/ParseUtils.rkt")
 (provide (contract-out [solution-part1 (-> string? integer?)]
                        [solution-part2 (-> string? integer?)]))
 
+(define-generics navigable
+  (move-dir navigable instr)
+  (move-forward navigable distance)
+  (rotate navigable instr)
+  (get-pos navigable))
+
 (struct point (x y))
-(struct ship (pos dir))
 
 (define input-parser
   (let* ([movement-parser (parser-cons (oneOf "NESWF") +integer)]
@@ -13,8 +20,8 @@
          [line-parser (<any> movement-parser rotation-parser)])
         (trim-spaces-eof (end-or-sep-by line-parser $eol))))
 
-(define/match (manhattan-distance pos)
-  [((point x y)) (+ (abs x) (abs y))])
+(define (manhattan-distance ship)
+  (match-let ([(point x y) (get-pos ship)]) (+ (abs x) (abs y))))
 
 (define/match (move instr pos)
   [((cons #\N distance) (point x y)) (point x (+ y distance))]
@@ -22,10 +29,31 @@
   [((cons #\S distance) (point x y)) (point x (- y distance))]
   [((cons #\W distance) (point x y)) (point (- x distance) y)])
 
+(define/match (execute-instruction instr ship)
+  [((cons (or #\N #\E #\S #\W) _) _) (move-dir ship instr)]
+  [((cons #\F distance) _) (move-forward ship distance)]
+  [((cons (or #\R #\L) _) _) (rotate ship instr)])
+
+(define (find-final-position input ship)
+  (~>> (parse-result input-parser input)
+       (foldl execute-instruction ship)
+       manhattan-distance))
+
 (define (solution-part1 input)
+
+  (struct ship (pos dir)
+    #:methods gen:navigable
+    [(define/match (move-dir _ship instr)
+       [((ship pos _) _) (struct-copy ship _ship [pos (move instr pos)])])
+     (define/match (move-forward _ship distance)
+       [((ship pos dir) _) (struct-copy ship _ship [pos (move (cons dir distance) pos)])])
+     (define/match (rotate _ship instr)
+       [((ship _ dir) _) (struct-copy ship _ship [dir (rotate-dir instr dir)])])
+     (define get-pos (λ~>> ship-pos))])
+
   (define direction-vector (vector-immutable #\N #\E #\S #\W))
 
-  (define (rotate instr dir)
+  (define (rotate-dir instr dir)
 
     (define (eval-direction rot-f degrees)
       (~>> (quotient degrees 90)
@@ -36,42 +64,33 @@
     (match instr [(cons #\R degrees) (eval-direction + degrees)]
                  [(cons #\L degrees) (eval-direction - degrees)]))
 
-  (define/match (execute-instruction instr ship_)
-    [((cons (or #\N #\E #\S #\W) _) (ship pos _)) (struct-copy ship ship_ [pos (move instr pos)])]
-    [((cons #\F distance) (ship pos dir)) (struct-copy ship ship_ [pos (move (cons dir distance) pos)])]
-    [((cons (or #\R #\L) _) (ship _ dir)) (struct-copy ship ship_ [dir (rotate instr dir)])])
-
-  (~>> (parse-result input-parser input)
-       (foldl execute-instruction (ship (point 0 0) #\E))
-       ship-pos
-       (manhattan-distance)))
+  (find-final-position input (ship (point 0 0) #\E)))
 
 (define (solution-part2 input)
 
-  (define/match (move-ship _ship waypoint distance)
-    [((ship (point sh-x sh-y) _) (point wp-x wp-y) _)
-     (struct-copy ship _ship [pos (point (+ (* distance wp-x) sh-x)
-                                         (+ (* distance wp-y) sh-y))])])
+  (struct ship (pos dir waypoint)
+    #:methods gen:navigable
+    [(define/match (move-dir _ship instr)
+       [((ship pos _ waypoint) _) (struct-copy ship _ship [waypoint (move instr waypoint)])])
+     (define/match (move-forward _ship distance)
+       [((ship pos _ waypoint) _) (struct-copy ship _ship [pos (move-to-waypoint pos waypoint distance)])])
+     (define/match (rotate _ship instr)
+       [((ship _ dir waypoint) _) (struct-copy ship _ship [waypoint (rotate-waypoint instr waypoint)])])
+     (define get-pos (λ~>> ship-pos))])
 
-  (define (rotate instr waypoint)
+  (define/match (move-to-waypoint pos waypoint distance)
+    [((point x y) (point wp-x wp-y) _) (point (+ (* distance wp-x) x) (+ (* distance wp-y) y))])
+
+  (define (rotate-waypoint instr waypoint)
     (match-define (point x y) waypoint)
 
-    (define (trig-fun fun theta)
-      (exact-round (fun (degrees->radians theta))))
+    (define (rotate-coord rot-fun degrees)
+      (let* ([z-init (make-rectangular x y)]
+             [rotated-angle (rot-fun (angle z-init) (degrees->radians degrees))]
+             [z-rotated (make-polar (magnitude z-init) rotated-angle)])
+            (point (exact-round (real-part z-rotated)) (exact-round (imag-part z-rotated)))))
 
-    (define (rotate-coord degrees)
-      (point (- (* (trig-fun cos degrees) x) (* (trig-fun sin degrees) y))
-             (+ (* (trig-fun sin degrees) x) (* (trig-fun cos degrees) y))))
+    (match instr [(cons #\R degrees) (rotate-coord - degrees)]
+                 [(cons #\L degrees) (rotate-coord + degrees)]))
 
-    (match instr [(cons #\R degrees) (rotate-coord (- 360 degrees))]
-                 [(cons #\L degrees) (rotate-coord degrees)]))
-
-  (define/match (execute-instruction instr ship-waypoint)
-    [((cons (or #\N #\E #\S #\W) _) (cons ship waypoint)) (cons ship (move instr waypoint))]
-    [((cons #\F distance) (cons ship waypoint)) (cons (move-ship ship waypoint distance) waypoint)]
-    [((cons (or #\R #\L) _) (cons ship waypoint)) (cons ship (rotate instr waypoint))])
-
-  (~>> (parse-result input-parser input)
-       (foldl execute-instruction (cons (ship (point 0 0) #\E) (point 10 1)))
-       ((λ~>> car ship-pos))
-       (manhattan-distance)))
+  (find-final-position input (ship (point 0 0) #\E (point 10 1))))
